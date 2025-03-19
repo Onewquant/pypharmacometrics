f7724e1862d56826d4f1e67383ad4d6f2dc1ca73
David Baek <seunghwan0216@gmail.com>
2025-03-20 00:28:15 +0900

    mc

--- KSCPT2025SPRWS_PD.py
+++ KSCPT2025SPRWS_PD.py
@@ -39,23 +39,28 @@ mdprep_conc_df = mdprep_conc_df.merge(adj_col_df, on=['ID','UID','GRP','TAD','TI
 mdprep_conc_df = mdprep_conc_df[mdprep_conc_df['ADJTAD'] <= 24].copy()
 mdprep_conc_df['Short_UID'] = mdprep_conc_df['UID'].map(lambda x:int(x[-2:]))
 mdprep_conc_df['UID'] = mdprep_conc_df['UID'].map(lambda x:x[1:])
+mdprep_conc_df['DV'] = mdprep_conc_df['DV'].astype(float)
 
 # mdprep_conc_df.to_csv("C:/Users/ilma0/PycharmProjects/pypharmacometrics/resource/KSCPTSPRWS25/modeling_prep_data/calculate_iAUC.csv", index=False, encoding='utf-8-sig')
 
 # mdprep_conc_df['TADADJ'] =
 
-# iAUC_df=pd.DataFrame({'Name':['T0-6','T6-12','T12-24'], 'Start':[0,6,12], 'End':[6,12,24]})
+iAUC_df=pd.DataFrame({'Name':['T[0-6h]','T[6-12h]','T[12-24h]','T[0-24h]'], 'Start':[0,6,12,0], 'End':[6,12,24,24]})
 # result = tblNCA(concData=mdprep_conc_df,key=['ID','DAY'],colTime="A_TIME",colConc='CONC',down = "Linear",iAUC=iAUC_df,dose=0.5,slopeMode='BEST',colStyle='pw')
-# iauc_result = tblNCA(concData=mdprep_conc_df,key=['ID','DAY'],colTime="ADJTIME",colConc='DV',down = "Log",iAUC=iAUC_df,dose=0.5,slopeMode='BEST',colStyle='pw')
-
+iauc_result = tblNCA(concData=mdprep_conc_df,key=['ID','UID','DAY'],colTime="ADJTIME",colConc='DV',down = "Log",iAUC=iAUC_df,dose=0.5,slopeMode='BEST',colStyle='pw')
+iauc_res_df = iauc_result.iloc[1:,:-1].melt(id_vars=list(iauc_result.iloc[:,:-1].columns[:-4]), var_name='AUCTIMEINT', value_name='IAUC')
 
 ## PD와 연결
 
-iauc_result = pd.read_csv(modeling_prepconc_dir_path+'/sglt2i_iauc.csv')
-iauc_result = iauc_result.rename(columns={'Short_UID':'UID'})
-iauc_result['UID'] = iauc_result['UID'].map(lambda x:'S'+str(x).zfill(3))
-iauc_res_df = iauc_result.melt(id_vars=list(iauc_result.columns[:-4]), var_name='AUCTIMEINT', value_name='IAUC')
+# iauc_result = pd.read_csv(modeling_prepconc_dir_path+'/sglt2i_iauc.csv')
+# iauc_result = iauc_result.rename(columns={'Short_UID':'UID'})
+# iauc_result['UID'] = iauc_result['UID'].map(lambda x:'S'+str(x).zfill(3))
+# iauc_res_df = iauc_result.melt(id_vars=list(iauc_result.columns[:-4]), var_name='AUCTIMEINT', value_name='IAUC')
+# iauc_result['AUClast'] = iauc_result['AUCLST']
+# if 'AUCLST' in iauc_res_df.columns: iauc_res_df['AUClast'] = iauc_result['AUCLST']
+
 iauc_res_df['AUCTIMEINT'] = iauc_res_df['AUCTIMEINT'].map(lambda x:x.split('[')[-1].split('h]')[0])
+iauc_res_df['N_Day'] = iauc_res_df['DAY'].copy()
 
 ## Blood PD dataset 정리
 
@@ -72,9 +77,9 @@ pg_zero_df = bpd_df[bpd_df['N_TIME']==0][['ID','DAY','C_Serum GLU']].rename(colu
 glu_nca_result = tblNCA(concData=bpd_df,key=['ID','DAY'],colTime="N_TIME",colConc='C_Serum GLU',down = "Log",dose=0.1,slopeMode='BEST',colStyle='pw')
 # glu_nca_result.columns
 s_glu_df = glu_nca_result[['ID','DAY','AUClast']].iloc[1:]
-s_glu_df['PG_AVG'] = s_glu_df['AUClast']/24
+s_glu_df['PG_AVG'] = s_glu_df['AUClast'].astype(float)/24
 s_glu_df = s_glu_df.merge(pg_zero_df, on=['ID','DAY'], how='left')
-s_glu_df = s_glu_df.rename(columns={'AUClast':'AUC_GLU','DAY':'N_Day'})
+s_glu_df = s_glu_df.rename(columns={'AUClast':'AUC_GLU','DAY':'N_Day','ID':'UID'})
 
 # bpd_df[bpd_df['ID']=='S031']
 
@@ -84,16 +89,16 @@ upd_df = pd.read_excel(f"{resource_dir_path}/PD urine_prep_Final_0707_prep_BASE 
 # upd_df = upd_df.iloc[1:].copy()
 # upd_df.columns
 upd_df = upd_df[upd_df['N_Time'].isin(['0-24','0-6','6-12','12-24'])]
-upd_df['UID'] = upd_df['ID']
+upd_df = upd_df.rename(columns={'ID':'UID'})
 upd_df['DAY'] = upd_df['N_Day']
 upd_df['GRP'] = upd_df['GRP'].map(int)
 upd_df['AUCTIMEINT'] = upd_df['N_Time']
 upd_df['Amount_GLU'] = upd_df['Amount_GLU'].astype(float)
 upd_df['Volume'] = upd_df['Volume'].astype(float)
-upd_addcol_df = upd_df.groupby(['ID','DAY']).agg({'Amount_GLU':'sum','Volume':'sum'}).reset_index(drop=False).rename(columns={'Amount_GLU':'UGE24','Volume':'VOLUME24'})
+upd_addcol_df = upd_df.groupby(['UID','DAY']).agg({'Amount_GLU':'sum','Volume':'sum'}).reset_index(drop=False).rename(columns={'Amount_GLU':'UGE24','Volume':'VOLUME24'})
 upd_baseline_df = upd_addcol_df[upd_addcol_df['DAY']==-1].copy().rename(columns={'UGE24':'BASELINE'}).drop(['DAY','VOLUME24'],axis=1)
-upd_addcol_df = upd_addcol_df.merge(upd_baseline_df, on=['ID'],how='left')
-upd_df = upd_df.merge(upd_addcol_df, on=['ID','DAY'],how='left')
+upd_addcol_df = upd_addcol_df.merge(upd_baseline_df, on=['UID'],how='left')
+upd_df = upd_df.merge(upd_addcol_df, on=['UID','DAY'],how='left')
 
 # daily auc만으로 정리
 upd_daily_df = upd_df.copy()
@@ -104,15 +109,96 @@ upd_daily_df['Amount_GLU'] = upd_daily_df['UGE24']
 upd_daily_df['AUCTIMEINT'] = upd_daily_df['N_Time']
 upd_daily_df = upd_daily_df.drop_duplicates(['UID','N_Day','N_Time'])
 
+
 # 공변량 붙이기
-covar_df = mdprep_conc_df.rename(columns={'ID':'NID','UID':'ID'})[['NID','ID']+list(mdprep_conc_df.columns)[7:15]].drop_duplicates(['ID'])
-upd_daily_df = upd_daily_df.merge(covar_df, on=['ID'],how='left')
-upd_daily_df = upd_daily_df.merge(s_glu_df, on=['ID','N_Day'],how='left')
+covar_df = mdprep_conc_df[['ID','UID']+list(mdprep_conc_df.columns)[7:15]].drop_duplicates(['ID'])
+upd_daily_df = upd_daily_df.merge(covar_df, on=['UID'],how='left')
+upd_daily_df = upd_daily_df.merge(s_glu_df, on=['UID','N_Day'],how='left')
+upd_daily_df = upd_daily_df.merge(iauc_res_df[['UID','N_Day','AUCTIMEINT','AUClast']], on=['UID','N_Day','AUCTIMEINT'],how='left')
+
+upd_daily_df['EFFECT0'] = (upd_daily_df['UGE24']-upd_daily_df['BASELINE'])/upd_daily_df['PG_ZERO']
+upd_daily_df['EFFECT1'] = (upd_daily_df['UGE24']-upd_daily_df['BASELINE'])/upd_daily_df['PG_AVG']
+upd_daily_df['EFFECTgfr'] = (upd_daily_df['UGE24']-upd_daily_df['BASELINE'])/(upd_daily_df['PG_AVG']*upd_daily_df['GFR'])
+
+upd_daily_df['GFR_MULTI_AUC'] = upd_daily_df['GFR']*upd_daily_df['AUClast']
+
+upd_daily_df.columns
+
 
 # EDA
-pre_df = upd_daily_df[upd_daily_df['DAY']==-1].copy()
-nss_df = upd_daily_df[upd_daily_df['DAY']==1].copy()
-ss_df = upd_daily_df[upd_daily_df['DAY']==7].copy()
+pre_df = upd_daily_df[upd_daily_df['DAY']==-1].sort_values(['GRP','UID'])
+nss_df = upd_daily_df[upd_daily_df['DAY']==1].sort_values(['GRP','UID'])
+ss_df = upd_daily_df[upd_daily_df['DAY']==7].sort_values(['GRP','UID'])
 
-nss_df['UGE24']
-nss_df['UGE24']
+## Multivariable linear regression
+
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+
+# 상관계수 행렬 계산
+corr_matrix = nss_df[['AGE', 'SEX', 'HT', 'WT', 'BMI', 'ALB', 'GFR', 'TBIL', 'AUC_GLU', 'PG_AVG', 'PG_ZERO', 'AUClast','GFR_MULTI_AUC']].corr().abs()
+
+# 상삼각행렬 추출 (자기 자신과 중복 제거)
+lower = corr_matrix.where(np.tril(np.ones(corr_matrix.shape), k=-1).astype(bool))
+
+# 상관계수 0.9 이상인 변수 중 하나 제거
+to_drop = [column for column in lower.columns if any(lower[column] > 0.7)]
+
+# 독립변수(X), 종속변수(y)
+total_cand = ['AGE', 'SEX', 'HT', 'WT', 'BMI', 'ALB', 'GFR', 'TBIL', 'AUC_GLU', 'PG_AVG', 'PG_ZERO', 'AUClast','GFR_MULTI_AUC']
+
+X = nss_df[list(set(total_cand)-set(to_drop))]
+for c in X.columns:
+    X[c]=X[c].astype(float)
+
+y = nss_df['UGE24']
+
+# VIF 계산
+vif_data = pd.DataFrame(columns=['Variable','VIF'])
+vif_data['Variable'] = list(X.columns)
+vif_data['VIF'] = [variance_inflation_factor(X.values, i) for i in range(X.shape[1])]
+no_vif_cols = list(vif_data[vif_data['VIF'] < 10]['Variable'])
+
+# 회귀모형 적합
+model = sm.OLS(y, X[no_vif_cols]).fit()
+
+# 결과 출력
+print(model.summary())
+
+
+# nss_df['EFFECT'] = (nss_df['UGE24']-nss_df['BASELINE'])/nss_df['PG_AVG']
+
+# nss_df.columns
+
+# scatter plot 그리기
+plt.figure(figsize=(15, 10))
+# x = 'GFR'
+x = 'GFR_MULTI_AUC'
+# y = 'EFFECTgfr'
+y = 'EFFECT0'
+# y = 'EFFECT1'
+hue = 'GRP'
+
+# sns.scatterplot(data=ss_df, x='AUClast', y='EFFECT0', hue='GRP')
+# sns.scatterplot(data=ss_df, x='AUClast', y='EFFECT1', hue='GRP')
+# sns.scatterplot(data=nss_df, x='AUClast', y='EFFECT0', hue='GRP')
+# sns.scatterplot(data=nss_df, x='AUClast', y='EFFECT1', hue='GRP')
+sns.scatterplot(data=nss_df, x=x, y=y, hue=hue)
+plt.title(f'{x} vs {y} by {hue}')
+plt.xlabel(x)
+plt.ylabel(y)
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+
+X = nss_df[[x]]
+X_const = sm.add_constant(X)
+y_vals = nss_df[y]
+
+model = sm.OLS(y_vals, X_const).fit()
+
+intercept, slope = model.params
+r_squared = model.rsquared
+p_value = model.pvalues[x]
