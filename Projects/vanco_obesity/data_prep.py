8d04cdafd38a70ae6e8f579d0196774563f0c521
onew <seunghwan0216@gmail.com>
2025-05-15 23:28:24 +0900

    mc

--- Projects/vanco_obesity/data_prep.py
+++ Projects/vanco_obesity/data_prep.py
@@ -117,8 +117,8 @@ for inx, df_frag in (md_df[md_df['MDV']==1]).groupby(['ID']):
     if time_interval_peak_exist > 0:
         irreg_interval_df.append(inx)
 
-irreg_interval_list = md_df[md_df['ID'].isin(irreg_interval_df)].reset_index(drop=True)
-irreg_interval_list.to_csv(f'{prj_dir}/resource/irreg_df.csv', index=False)
+irreg_interval_df = md_df[md_df['ID'].isin(irreg_interval_df)].reset_index(drop=True)
+irreg_interval_df.to_csv(f'{prj_dir}/resource/irreg_df.csv', index=False)
 
 """
 # (1) Dose 없이 DV가 0이 아닌 측정 데이터가 먼저 나오는 사람들이 있다 (TIME < 0)
@@ -159,6 +159,12 @@ lab_df["VALUE"] = val_list
 # lab code <-> lab name 변환 dict 생성
 lab_rename_dict = dict([(row['LAB_CODE'],row['LAB']) for inx, row in lab_df[['LAB_CODE','LAB']].drop_duplicates(['LAB_CODE']).iterrows()])
 
+"""
+# {'L2002': 'RBC', 'L2001': 'WBC', 'L201218': 'ANC', 'L2009': 'PLT', 'L2004': 'Hct', 'L2003': 'Hb', 'L6108': 'Bilirubin', 'L6109': 'Blood', 'L6110': 'Urobilinogen', 'L6111': 'Nitrite', 'L6112': 'WBC stick', 'L61301': 'RBC', 'L61302': 'WBC', 'L61306': 'Bacteria', 'L61021': 'Color', 'L61022': 'Turbidity', 'L61309': 'Others', 'L6103': 'Specific Gravity', 'L6104': 'pH', 'L6105': 'Protein', 'L6106': 'Glucose', 'L6107': 'Ketone', 'L8186': 'CRP', 'L8136': 'Bilirubin, total', 'L81354': 'eGFR-Cockcroft-Gault', 'L81353': 'eGFR-Schwartz', 'L81352': 'eGFR-CKD-EPI', 'L81351': 'eGFR', 'L8135': 'Creatinine', 'L8134': 'BUN', 'L8164': 'Uric Acid', 'L8166': 'Alkaline Phosphotase', 'L8163': 'Albumin', 'L8162': 'Protein, total', 'L8153': 'ALT', 'L8152': 'AST', 'L8165': 'Cholesterol'}
+소변검사 여부 확인
+"""
+
+
 # 가장 가까운 검사일자 찾는 함수
 mlab_df = lab_df.merge(id_df[['ID','DATETIME']].rename(columns={'DATETIME':'TDM_DT'}), on=['ID'], how='left')
 nearTDM_lab_df = mlab_df[mlab_df['DATETIME'] <= mlab_df['TDM_DT']].sort_values(['ID','DATETIME']).groupby(['ID','LAB'], as_index=False).agg({'DATETIME':'last','VALUE':'last'})
@@ -171,19 +177,21 @@ nearTDM_lab_df.columns.name = None
 # lab_results = id_df.apply(lambda row:mlab_df get_latest_lab_result(row, lab_df), axis=1)
 # idlab_df = pd.concat([id_df, lab_results], axis=1)
 # idlab_df.rename(columns=lab_rename_dict, inplace=True)
-
+# id_df.columns
 # md_df와 df_idlab을 'ID' 기준으로 병합
-final_df = md_df.merge(nearTDM_lab_df, how='left', on=['ID']).merge(id_df[['ID','H']], how='left', on=['ID'])
+final_df = md_df.merge(id_df[['ID','SEX','AGE','HT','WT','BMI','OB','LBM']], how='left', on=['ID']).merge(nearTDM_lab_df, how='left', on=['ID'])
 # nearTDM_lab_df['ID'].iloc[0]
 # md_df['ID'].iloc[0]
-final_df.columns
+# final_df.columns
+final_df = final_df.rename(columns={'Bilirubin':'TBil','eGFR-CKD-EPI':'eGFR_CE','eGFR-Schwartz':'eGFR_Shw','eGFR':'eGFR_MDRD','Alkaline Phosphotase':'ALP','Albumin':'ALB','Creatinine':'Cr', 'eGFR-Cockcroft-Gault':'eGFR_CG'})
 
+final_df.columns
 # 중복 컬럼 제거
 final_df = final_df[['ID', 'TIME', 'DV', 'MDV', 'CMT', 'AMT', 'RATE', 'SEX', 'AGE', 'HT', 'WT', 'BMI',
                      'OB', 'LBM', 'eGFR_CE', 'eGFR_Shw', 'AST', 'WBC', 'PLT', 'eGFR_MDRD', 'CRP', 'TBil',
-                     'ALP', 'ALB', 'Hct', 'pH', 'RBC', 'ALT', 'ANC', 'Tpro', 'Cr', 'BUN', 'eGFR_CG', 'UA', 'Hb']]
+                     'ALP', 'ALB', 'Hct', 'pH', 'RBC', 'ALT', 'ANC', 'Cr', 'BUN', 'eGFR_CG', 'Hb']]
 
-final_df.to_csv('/Users/ssoroomii/Desktop/LSH/03. Paper/01. VCM/CSR2 data/raw data/final_df.csv', index=False)
+final_df.to_csv(f'{prj_dir}/resource/final_df.csv', index=False)
 
 # 결과 출력
-print(final_df.head(50))
+# print(final_df.head(50))
