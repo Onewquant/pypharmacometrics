b6f178d2248c5da3cbe9e2f48914a25dec6167f4
onew <seunghwan0216@gmail.com>
2025-06-14 01:52:02 +0900

    mc

--- Projects/IBD_PGx/modeling_covar_addon.py
+++ Projects/IBD_PGx/modeling_covar_addon.py
@@ -67,22 +67,22 @@ for drug in ['infliximab','adalimumab']:
 
         ## Covariates의 NA value 처리 (ffill 먼저 시도, 없으면 bfill, 그것도 없으면 전체의 median 값)
 
-        # md_df_list = list()
-        # for md_inx,md_df in modeling_df.groupby(['UID']):
-        #     md_df = md_df.sort_values(['DATETIME']).fillna(method='ffill').fillna(method='bfill')
-        #     md_df_list.append(md_df)
-        # modeling_df = pd.concat(md_df_list).reset_index(drop=True)
-        #
-        # modeling_df.fillna(modeling_df.median(numeric_only=True), inplace=True)
+        md_df_list = list()
+        for md_inx,md_df in modeling_df.groupby(['UID']):
+            md_df = md_df.sort_values(['DATETIME']).fillna(method='ffill').fillna(method='bfill')
+            md_df_list.append(md_df)
+        modeling_df = pd.concat(md_df_list).reset_index(drop=True)
+
+        modeling_df.fillna(modeling_df.median(numeric_only=True), inplace=True)
 
         ## Covariates의 NA value 처리 (ffill 먼저 시도, 없으면 bfill, 그것도 없으면 전체의 median 값)
 
-        md_df_list = list()
-        for md_inx, md_df in modeling_df.groupby(['UID']):
-            # md_df = md_df.sort_values(['DATETIME']).fillna(method='ffill')
-            md_df = md_df.sort_values(['DATETIME'])
-            md_df_list.append(md_df)
-        modeling_df = pd.concat(md_df_list).reset_index(drop=True)
+        # md_df_list = list()
+        # for md_inx, md_df in modeling_df.groupby(['UID']):
+        #     # md_df = md_df.sort_values(['DATETIME']).fillna(method='ffill')
+        #     md_df = md_df.sort_values(['DATETIME'])
+        #     md_df_list.append(md_df)
+        # modeling_df = pd.concat(md_df_list).reset_index(drop=True)
 
         # modeling_df.fillna('.', inplace=True)
 
@@ -108,7 +108,14 @@ for drug in ['infliximab','adalimumab']:
         #     modeling_df["time_decrease"] = (modeling_df["TIME"] < modeling_df["prev_time"])
         #     problem_rows = modeling_df[modeling_df["time_decrease"]]
         #     print(problem_rows)
-        # raise ValueError
+
+        # modeling_df['CALPRTSTL'].median()
+        # modeling_df['CREATININE'].median()
+        # modeling_df['ALB'].median()
+        # modeling_df['CRP'].median()
+        raise ValueError
+
+        # modeling_df['A_0FLG'] = (modeling_df['ID'].shift(1)!=modeling_df['ID'])*1
 
         modeling_df.to_csv(f'{output_dir}/{drug}_{mode_str}_modeling_df.csv',index=False, encoding='utf-8-sig')
         modeling_df[~((modeling_df['TIME'] == 0) & (modeling_df['DV'] == '0.0'))].copy().to_csv(f'{output_dir}/{drug}_{mode_str}_modeling_df_without_zero_dv.csv',index=False, encoding='utf-8-sig')
--- Projects/INTHERA/INTH_eda.py
+++ Projects/INTHERA/INTH_eda.py
@@ -1,19 +1,66 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 from pynca.tools import *
+import statsmodels.api as sm
 
 prj_dir = 'C:/Users/ilma0/PycharmProjects/pypharmacometrics/Projects/INTHERA'
 df = pd.read_csv(f"{prj_dir}/in1011_id.csv")
 
 # comp_dict = {1:'Blood',2:'cDRG',3:'lDRG',4:'tDRG',5:'Heart',6:"Kidney",7:'Brain',8:'Liver',9:'Lung',10:'Stomach',11:'Femur'}
 # comp_dict = {1:'Blood',2:'cDRG',3:'lDRG',4:'tDRG',5:'Heart',6:'Intestine',7:"Kidney",8:'Brain',9:'Liver',10:'Lung',11:'Stomach',12:'Femur',13:'UB',14:'GB'}
-comp_dict = {5:'Blood',2:'cDRG',3:'lDRG',4:'tDRG',1:'Heart',6:'Intestine',7:"Kidney",8:'Brain',9:'Liver',10:'Lung',11:'Stomach',12:'Femur',13:'UB',14:'GB'}
+comp_dict = {1:'Blood',2:'cDRG',3:'lDRG',4:'tDRG',5:'Heart',6:'Intestine',7:"Kidney",8:'Brain',9:'Liver',10:'Lung',11:'Stomach',12:'Femur',13:'UB',14:'GB'}
 
 # rev_comp_dict =
 
 gdf = df.copy()
 gdf['CMT'] = gdf['CMT'].map(comp_dict)
+
+fdf = gdf[(gdf['MDV']==0)].copy()
+
+fdf['DV'] = fdf['DV'].astype(float)
+fdf = fdf[['ID','TIME','CMT','DV']].pivot(index=['ID','TIME'],columns='CMT',values='DV').reset_index(drop=False)
+fdf = fdf[fdf['TIME']!=0].copy()
+
+# 독립변수(Blood)와 종속변수(Heart) 정의
+X = fdf['Blood']
+y = fdf['Heart']
+
+# 상수항 추가
+X = sm.add_constant(X)
+
+# 모델 피팅
+model = sm.OLS(y, X).fit()
+
+# 회귀 결과 출력
+print(model.summary())
+
+# 상관도 그리기
+sns.regplot(x=np.log(X['Blood']+0.1), y=y, data=df, ci=95, line_kws={'color': 'red'}, scatter_kws={'alpha': 0.6})
+
+plt.title("Scatter plot with Regression Line")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid(True)
+plt.show()
+
+
+
+cdf = fdf.loc[:,'Blood':].copy()
+# sns.heatmap(cdf.corr(), annot=True)
+
+# 상관계수 계산
+corr = cdf.corr()
+
+# 히트맵 그리기
+plt.figure(figsize=(12, 10))
+sns.heatmap(corr, annot=True, fmt=".2f", cmap="coolwarm", vmin=-1, vmax=1, square=True)
+plt.title("Correlation Matrix of All Columns")
+plt.show()
+
+
+
 
 # ## 그래프 그리기
 #
