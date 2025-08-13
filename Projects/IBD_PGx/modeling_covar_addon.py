d484d97ee3f8ea8e8f71d544bf5f2d798397a05a
onew <seunghwan0216@gmail.com>
2025-08-13 23:32:02 +0900

    mc

--- Projects/IBD_PGx/modeling_covar_addon.py
+++ Projects/IBD_PGx/modeling_covar_addon.py
@@ -24,104 +24,109 @@ demo_df['UID'] = demo_df['UID'].astype(str)
 # demo2_df = demo2_df[['UID','HT', 'WT', 'BMI','BSA']].copy()
 # demo2_df['UID'] = demo2_df['UID'].astype(str)
 
-## WT, HT, ADHERENCE Covariates / PD markers Loading
-
-pd_marker_list = ['PD_PRO2', 'PD_STLFREQ', 'PD_ABDPAIN', 'PD_RECTBLD','PD_TOTALSCORE']
-
-cdai_hx_df = pd.read_csv(f"{output_dir}/pdmarker_cdai_df.csv")
-cdai_hx_df = cdai_hx_df.rename(columns={'ID':'UID','DATE':'DATETIME','CDAI_TOTALSCORE':'PD_TOTALSCORE','CDAI_ADHERENCEPCT':'ADHERENCE','CDAI_WEIGHT':'WT','CDAI_HEIGHT':'HT', 'CDAI_BMI':'BMI'})
-cdai_hx_df['UID'] = cdai_hx_df['UID'].astype(str)
-cdai_hx_df['PD_PRO2'] = 2*cdai_hx_df['CDAI_DIARHEACNT'] + 5*cdai_hx_df['CDAI_ABDPAINCNT']
-cdai_hx_df['PD_STLFREQ'] = cdai_hx_df['CDAI_DIARHEACNT']
-cdai_hx_df['PD_ABDPAIN'] = cdai_hx_df['CDAI_ABDPAINCNT']
-cdai_hx_df['PD_RECTBLD'] = np.nan
-cdai_hx_df['IBD_TYPE'] = 'CD'
-# cdai_adh_df.columns
-
-pms_hx_df = pd.read_csv(f"{output_dir}/pdmarker_pms_df.csv")
-pms_hx_df = pms_hx_df.rename(columns={'ID':'UID'}).rename(columns={'ID':'UID','DATE':'DATETIME','PMS_TOTALSCORE':'PD_TOTALSCORE','PMS_ADHERENCEPCT':'ADHERENCE','PMS_WEIGHT':'WT','PMS_HEIGHT':'HT', 'PMS_BMI':'BMI'})
-pms_hx_df['UID'] = pms_hx_df['UID'].astype(str)
-pms_hx_df['PD_PRO2'] = pms_hx_df['PMS_STLCNT'] + pms_hx_df['PMS_HEMATOCHEZIA']
-pms_hx_df['PD_STLFREQ'] = pms_hx_df['PMS_STLCNT']
-pms_hx_df['PD_ABDPAIN'] = np.nan
-pms_hx_df['PD_RECTBLD'] = pms_hx_df['PMS_HEMATOCHEZIA']
-pms_hx_df['IBD_TYPE'] = 'UC'
-# pms_adh_df.columns
-
-adh_df = pd.concat([cdai_hx_df[['UID','DATETIME','ADHERENCE','IBD_TYPE'] + pd_marker_list], pms_hx_df[['UID','DATETIME','ADHERENCE','IBD_TYPE']+pd_marker_list]]).drop_duplicates(['UID','DATETIME']).sort_values(['UID','DATETIME'])
-bsize_df = pd.concat([cdai_hx_df[['UID','DATETIME','WT','HT','BMI']], pms_hx_df[['UID','DATETIME','WT','HT','BMI']]]).drop_duplicates(['UID','DATETIME']).sort_values(['UID','DATETIME'])
-# adh_df[adh_df['WT'].isna()]
-# adh_df[adh_df['HT'].isna()]
-
-full_result_df = list()
-count = 0
-for uid, uid_df in adh_df.groupby('UID',as_index=False): #break
-
-    min_lab_date = uid_df['DATETIME'].min()
-    max_lab_date = uid_df['DATETIME'].max()
-
-    print(f"({count}) {uid} / Date Range : ({min_lab_date},  {max_lab_date})")
-
-    uid_fulldt_df = pd.DataFrame(columns=['UID','DATETIME'])
-    uid_fulldt_df['DATETIME'] = pd.date_range(start=min_lab_date,end=max_lab_date).astype(str)
-    uid_fulldt_df['UID'] = uid
-
-    # uid_fulldt_df = uid_fulldt_df.merge(uid_df, on=['UID','DATETIME'], how='left').fillna(method='bfill')
-    uid_fulldt_df = uid_fulldt_df.merge(uid_df, on=['UID','DATETIME'], how='left')
-    # uid_fulldt_df = uid_fulldt_df.merge(uid_df, on=['UID','DATETIME'], how='left')
-
-    if count==0:
-        full_result_df = uid_fulldt_df.copy()
-    else:
-        full_result_df = pd.concat([full_result_df, uid_fulldt_df.copy()])
-
-    count+=1
-
-# raise ValueError
-adh_df = full_result_df.copy()
-# pdm_med_uc = adh_df[adh_df['IBD_TYPE']=='UC']['PD_MARKER'].median()
-# pdm_med_cd = adh_df[adh_df['IBD_TYPE']=='CD']['PD_MARKER'].median()
+# ## WT, HT, ADHERENCE Covariates / PD markers Loading
 #
-# pdpro2_med_uc = adh_df[adh_df['IBD_TYPE']=='UC']['PD_PRO2'].median()
-# pdpro2_med_cd = adh_df[adh_df['IBD_TYPE']=='CD']['PD_PRO2'].median()
+# pd_marker_list = ['PD_PRO2', 'PD_STLFREQ', 'PD_ABDPAIN', 'PD_RECTBLD','PD_TOTALSCORE']
 #
-# med_pd_value = {'UC':pdm_med_uc,'CD':pdm_med_cd}
-# full_result_df['PD_MARKER'] = full_result_df.apply(lambda row:row['PD_MARKER'] if not np.isnan(row['PD_MARKER']) else med_pd_value[row['IBD_TYPE']], axis=1)
-# full_result_df['PD_PRO2'] = full_result_df.apply(lambda row:row['PD_PRO2'] if not np.isnan(row['PD_PRO2']) else 7777777, axis=1)
-# adh_df = full_result_df.fillna(full_result_df.median(numeric_only=True))
-# adh_df['PD_PRO2'] = adh_df['PD_PRO2'].replace(7777777,np.nan)
-
-adh_df = adh_df.drop('IBD_TYPE', axis=1)
-
-
-full_result_df = list()
-count = 0
-for uid, uid_df in bsize_df.groupby('UID',as_index=False): #break
-
-    min_lab_date = uid_df['DATETIME'].min()
-    max_lab_date = uid_df['DATETIME'].max()
-
-    print(f"({count}) {uid} / Date Range : ({min_lab_date},  {max_lab_date})")
-
-    uid_fulldt_df = pd.DataFrame(columns=['UID','DATETIME'])
-    uid_fulldt_df['DATETIME'] = pd.date_range(start=min_lab_date,end=max_lab_date).astype(str)
-    uid_fulldt_df['UID'] = uid
-
-    uid_fulldt_df = uid_fulldt_df.merge(uid_df, on=['UID','DATETIME'], how='left').fillna(method='ffill')
-    # uid_fulldt_df = uid_fulldt_df.merge(uid_df, on=['UID','DATETIME'], how='left')
-
-    if count==0:
-        full_result_df = uid_fulldt_df.copy()
-    else:
-        full_result_df = pd.concat([full_result_df, uid_fulldt_df.copy()])
-
-    count+=1
-bsize_df = full_result_df.fillna(full_result_df.median(numeric_only=True))
+# cdai_hx_df = pd.read_csv(f"{output_dir}/pdmarker_cdai_df.csv")
+# cdai_hx_df = cdai_hx_df.rename(columns={'ID':'UID','DATE':'DATETIME','CDAI_TOTALSCORE':'PD_TOTALSCORE','CDAI_ADHERENCEPCT':'ADHERENCE','CDAI_WEIGHT':'WT','CDAI_HEIGHT':'HT', 'CDAI_BMI':'BMI'})
+# cdai_hx_df['UID'] = cdai_hx_df['UID'].astype(str)
+# cdai_hx_df['PD_PRO2'] = 2*cdai_hx_df['CDAI_DIARHEACNT'] + 5*cdai_hx_df['CDAI_ABDPAINCNT']
+# cdai_hx_df['PD_STLFREQ'] = cdai_hx_df['CDAI_DIARHEACNT']
+# cdai_hx_df['PD_ABDPAIN'] = cdai_hx_df['CDAI_ABDPAINCNT']
+# cdai_hx_df['PD_RECTBLD'] = np.nan
+# cdai_hx_df['IBD_TYPE'] = 'CD'
+# # cdai_adh_df.columns
+#
+# pms_hx_df = pd.read_csv(f"{output_dir}/pdmarker_pms_df.csv")
+# pms_hx_df = pms_hx_df.rename(columns={'ID':'UID'}).rename(columns={'ID':'UID','DATE':'DATETIME','PMS_TOTALSCORE':'PD_TOTALSCORE','PMS_ADHERENCEPCT':'ADHERENCE','PMS_WEIGHT':'WT','PMS_HEIGHT':'HT', 'PMS_BMI':'BMI'})
+# pms_hx_df['UID'] = pms_hx_df['UID'].astype(str)
+# pms_hx_df['PD_PRO2'] = pms_hx_df['PMS_STLCNT'] + pms_hx_df['PMS_HEMATOCHEZIA']
+# pms_hx_df['PD_STLFREQ'] = pms_hx_df['PMS_STLCNT']
+# pms_hx_df['PD_ABDPAIN'] = np.nan
+# pms_hx_df['PD_RECTBLD'] = pms_hx_df['PMS_HEMATOCHEZIA']
+# pms_hx_df['IBD_TYPE'] = 'UC'
+# # pms_adh_df.columns
+#
+# adh_df = pd.concat([cdai_hx_df[['UID','DATETIME','ADHERENCE','IBD_TYPE'] + pd_marker_list], pms_hx_df[['UID','DATETIME','ADHERENCE','IBD_TYPE']+pd_marker_list]]).drop_duplicates(['UID','DATETIME']).sort_values(['UID','DATETIME'])
+# bsize_df = pd.concat([cdai_hx_df[['UID','DATETIME','WT','HT','BMI']], pms_hx_df[['UID','DATETIME','WT','HT','BMI']]]).drop_duplicates(['UID','DATETIME']).sort_values(['UID','DATETIME'])
+# # adh_df[adh_df['WT'].isna()]
+# # adh_df[adh_df['HT'].isna()]
+#
+# full_result_df = list()
+# count = 0
+# for uid, uid_df in adh_df.groupby('UID',as_index=False): #break
+#
+#     min_lab_date = uid_df['DATETIME'].min()
+#     max_lab_date = uid_df['DATETIME'].max()
+#
+#     print(f"({count}) {uid} / Date Range : ({min_lab_date},  {max_lab_date})")
+#
+#     uid_fulldt_df = pd.DataFrame(columns=['UID','DATETIME'])
+#     uid_fulldt_df['DATETIME'] = pd.date_range(start=min_lab_date,end=max_lab_date).astype(str)
+#     uid_fulldt_df['UID'] = uid
+#
+#     # uid_fulldt_df = uid_fulldt_df.merge(uid_df, on=['UID','DATETIME'], how='left').fillna(method='bfill')
+#     uid_fulldt_df = uid_fulldt_df.merge(uid_df, on=['UID','DATETIME'], how='left')
+#     # uid_fulldt_df = uid_fulldt_df.merge(uid_df, on=['UID','DATETIME'], how='left')
+#
+#     if count==0:
+#         full_result_df = uid_fulldt_df.copy()
+#     else:
+#         full_result_df = pd.concat([full_result_df, uid_fulldt_df.copy()])
+#
+#     count+=1
+#
+# # raise ValueError
+# adh_df = full_result_df.copy()
+# # pdm_med_uc = adh_df[adh_df['IBD_TYPE']=='UC']['PD_MARKER'].median()
+# # pdm_med_cd = adh_df[adh_df['IBD_TYPE']=='CD']['PD_MARKER'].median()
+# #
+# # pdpro2_med_uc = adh_df[adh_df['IBD_TYPE']=='UC']['PD_PRO2'].median()
+# # pdpro2_med_cd = adh_df[adh_df['IBD_TYPE']=='CD']['PD_PRO2'].median()
+# #
+# # med_pd_value = {'UC':pdm_med_uc,'CD':pdm_med_cd}
+# # full_result_df['PD_MARKER'] = full_result_df.apply(lambda row:row['PD_MARKER'] if not np.isnan(row['PD_MARKER']) else med_pd_value[row['IBD_TYPE']], axis=1)
+# # full_result_df['PD_PRO2'] = full_result_df.apply(lambda row:row['PD_PRO2'] if not np.isnan(row['PD_PRO2']) else 7777777, axis=1)
+# # adh_df = full_result_df.fillna(full_result_df.median(numeric_only=True))
+# # adh_df['PD_PRO2'] = adh_df['PD_PRO2'].replace(7777777,np.nan)
+#
+# adh_df = adh_df.drop('IBD_TYPE', axis=1)
+#
+#
+# full_result_df = list()
+# count = 0
+# for uid, uid_df in bsize_df.groupby('UID',as_index=False): #break
+#
+#     min_lab_date = uid_df['DATETIME'].min()
+#     max_lab_date = uid_df['DATETIME'].max()
+#
+#     print(f"({count}) {uid} / Date Range : ({min_lab_date},  {max_lab_date})")
+#
+#     uid_fulldt_df = pd.DataFrame(columns=['UID','DATETIME'])
+#     uid_fulldt_df['DATETIME'] = pd.date_range(start=min_lab_date,end=max_lab_date).astype(str)
+#     uid_fulldt_df['UID'] = uid
+#
+#     uid_fulldt_df = uid_fulldt_df.merge(uid_df, on=['UID','DATETIME'], how='left').fillna(method='ffill')
+#     # uid_fulldt_df = uid_fulldt_df.merge(uid_df, on=['UID','DATETIME'], how='left')
+#
+#     if count==0:
+#         full_result_df = uid_fulldt_df.copy()
+#     else:
+#         full_result_df = pd.concat([full_result_df, uid_fulldt_df.copy()])
+#
+#     count+=1
+# bsize_df = full_result_df.fillna(full_result_df.median(numeric_only=True))
 
 # adh_df['ADHERENCE'].drop_duplicates()
 # adh_df['UID'].drop_duplicates()
 # adh_df.to_csv(f"{output_dir}/adherence_df.csv", encoding='utf-8-sig', index=False)
 
+## PD_BODY SIZE Lading
+
+pd_bsize_df = pd.read_csv(f"{output_dir}/pd_bsize_df.csv")
+pd_marker_list = pd.DataFrame(pd_bsize_df.columns)[0]
+pd_marker_list = list(pd_marker_list[pd_marker_list.map(lambda x: 'PD_' in x)])
 
 ## LAB Covariates Loading
 
@@ -169,9 +174,9 @@ for drug in ['infliximab','adalimumab']:
         # modeling_df
         raw_modeling_df = raw_modeling_df.merge(totlab_df, on=['UID','DATETIME'], how='left')
         raw_modeling_df = raw_modeling_df.merge(demo_df, on=['UID'], how='left')
-        # modeling_df = modeling_df.merge(demo2_df, on=['UID'], how='left')
-        raw_modeling_df = raw_modeling_df.merge(adh_df, on=['UID','DATETIME'], how='left')
-        raw_modeling_df = raw_modeling_df.merge(bsize_df, on=['UID','DATETIME'], how='left')
+        # raw_modeling_df = raw_modeling_df.merge(adh_df, on=['UID','DATETIME'], how='left')
+        # raw_modeling_df = raw_modeling_df.merge(bsize_df, on=['UID','DATETIME'], how='left')
+        raw_modeling_df = raw_modeling_df.merge(pd_bsize_df, on=['UID','DATETIME'], how='left')
 
         # modeling_df['UID'].iloc[0]
         # demo_df['UID'].iloc[0]
